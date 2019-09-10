f1f65c7c3ad085bf489a14b15ca9eef3d1f2ac31
letara48 <lgirl96@hotmail.com>
2019-09-10 16:47:16 -0400

    Reverse strings in Array

---
+++ src/Sarrays.java
@@ -0,0 +1,14 @@
+public class Sarrays {
+    public static void main(String[] args) {
+        String input = "emocleW";
+
+        char [] greeting = input.toCharArray();
+
+        for (int i = greeting.length-1; i >= 0; i--){
+           System.out.print(greeting[i]);
+
+
+        }
+
+    }
+}
